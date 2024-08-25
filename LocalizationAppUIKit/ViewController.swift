7d98aac6c8c6c8cff7856c8bd2e5ee42d5fe74a1
Galang Aji Susanto <galang.aji@mekari.com>
2024-08-25 20:44:41 +0700

    Add string catalog for localizations and regions

--- LocalizationAppUIKit/ViewController.swift
+++ LocalizationAppUIKit/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message1Label: UILabel!
+    @IBOutlet weak var message2Label: UILabel!
+    @IBOutlet weak var message3Label: UILabel!
+    
+    let name = "Galang"
+    let applicationCount = 6
+    let yearExperience = 3.5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        message1Label.text = String(localized: "Hello, world! My name is \(name)")
+        message2Label.text = String(localized: "I love coding")
+        message3Label.text = String(localized: "After \(yearExperience, specifier: "%.1f") year(s) experience, i have released \(applicationCount) application(s)")
     }
 
 
